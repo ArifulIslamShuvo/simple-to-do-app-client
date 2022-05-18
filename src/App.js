d791dc325240b6d5fd3277127e2a2931e456570e
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-05-19 00:15:59 +0600

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>welcome to-do-app</h2>
+      <button class="btn btn-outline btn-success">Success</button>
+    </div>
+  );
+}
+
+export default App;
